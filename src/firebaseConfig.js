d5a6b669ca3f1ff92feefc1aa541f3cd10bf016f
Yuto <utouto97@gmail.com>
2021-12-16 00:00:09 +0900

    firebaseの初期化処理を追加

---
+++ src/firebaseConfig.js
@@ -0,0 +1,13 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
+};
+
+export default firebaseConfig;
--- src/main.js
+++ src/main.js
@@ -2,4 +2,9 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 
+import { initializeApp } from "firebase/app";
+import firebaseConfig from "@/firebaseConfig";
+
+initializeApp(firebaseConfig);
+
 createApp(App).use(router).mount("#app");
