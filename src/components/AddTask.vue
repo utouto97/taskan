8fca7d590ff127d6fc7dbc8321f20f91781924db
Yuto <utouto97@gmail.com>
2021-12-24 20:13:14 +0900

    タスク追加の場所として先頭か末尾を選択可能に

--- src/components/AddTask.vue
+++ src/components/AddTask.vue
@@ -8,6 +8,14 @@
       <label for="description">説明</label>
       <input type="text" id="description" v-model="description" />
     </div>
+    <div>
+      <label for="order">いつやる？</label>
+      <select v-model="order">
+        <option disabled value="">Please select one</option>
+        <option value="first">今</option>
+        <option value="last">後で</option>
+      </select>
+    </div>
     <button @click="addTask()">追加</button>
   </div>
 </template>
@@ -29,32 +37,59 @@ export default {
   setup() {
     const title = ref("");
     const description = ref("");
+    const order = ref("");
+
     const db = getFirestore();
     const { user } = useAuth();
 
     const addTask = async () => {
       const userDoc = await getDoc(doc(db, "users", user.value.uid));
       const headTask = userDoc.data().headTask;
-      const newTask = await addDoc(
-        collection(db, "users", user.value.uid, "tasks"),
-        {
-          title: title.value,
-          description: description.value,
-          prev: "head",
-          next: headTask,
-        }
-      );
-      await updateDoc(doc(db, "users", user.value.uid, "tasks", headTask), {
-        prev: newTask.id,
-      });
-      await updateDoc(doc(db, "users", user.value.uid), {
-        headTask: newTask.id,
-      });
+      const tailTask = userDoc.data().tailTask;
+
+      if (order.value == "first") {
+        const newTask = await addDoc(
+          collection(db, "users", user.value.uid, "tasks"),
+          {
+            title: title.value,
+            description: description.value,
+            prev: "head",
+            next: headTask,
+          }
+        );
+
+        await updateDoc(doc(db, "users", user.value.uid, "tasks", headTask), {
+          prev: newTask.id,
+        });
+
+        await updateDoc(doc(db, "users", user.value.uid), {
+          headTask: newTask.id,
+        });
+      } else if (order.value == "last") {
+        const newTask = await addDoc(
+          collection(db, "users", user.value.uid, "tasks"),
+          {
+            title: title.value,
+            description: description.value,
+            prev: tailTask,
+            next: "tail",
+          }
+        );
+
+        await updateDoc(doc(db, "users", user.value.uid, "tasks", tailTask), {
+          next: newTask.id,
+        });
+
+        await updateDoc(doc(db, "users", user.value.uid), {
+          tailTask: newTask.id,
+        });
+      }
     };
 
     return {
       title,
       description,
+      order,
       addTask,
     };
   },
