01e21c9a82f1398ce91b9a728abb874ffe8aaea6
Yuto <utouto97@gmail.com>
2021-12-24 15:54:31 +0900

    firebaseConfigを公開

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,13 +1,13 @@
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
-  measurementId: "",
+  apiKey: "AIzaSyDWVUyq0RQQIfB2JriiP-HjRcy1bAWG4U4",
+  authDomain: "taskan-f26db.firebaseapp.com",
+  projectId: "taskan-f26db",
+  storageBucket: "taskan-f26db.appspot.com",
+  messagingSenderId: "400080196603",
+  appId: "1:400080196603:web:d600246da88753c859f15e",
+  measurementId: "G-T0T0GTPC50",
 };
 
 export default firebaseConfig;
